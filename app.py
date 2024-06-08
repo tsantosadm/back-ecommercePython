3b09a47b9b1e0f25a26e3c48e5573ecda6faa947
Talisson Santos <tsantosadm@gmail.com>
2024-06-08 18:15:06 -0300

    user authentication

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin, login_user, logout_user, LoginManager, login_required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'minha_Chave_123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+CORS(app)
 
-#Modelagem
+#Modelagem User
+#User (id, name, password)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=True)
+    password = db.Column(db.String(80), nullable=True)
+
+
+#Modelagem Product
 #Produto (id, name, price, description)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,8 +29,34 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
     
+#Authentication
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#Definição de rota para Login de Usuário
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    
+    user = User.query.filter_by(username=data.get('username')).first()
+    
+    if user and data.get('password') == user.password:
+            login_user(user)
+            return jsonify({"message": "Logged in successfully"}), 200
+    return jsonify({"message": "Unauthorized. Invalid credencials"}), 401
+
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout successfully"})
+
+
 #Definição de rota para requisitar cadastro
 @app.route('/api/products/add', methods=['POST'])
+@login_required
 def add_product():
     data = request.json
     if 'name' in data and 'price' in data:
@@ -26,6 +67,7 @@ def add_product():
     return jsonify({"message": "Invalid product data"}), 400
 
 @app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
+@login_required
 def delete_product(product_id):
     product = Product.query.get(product_id)
     if product:
@@ -49,6 +91,7 @@ def get_product_details(product_id):
 
 
 @app.route('/api/products/update/<int:product_id>', methods=['PUT'])
+@login_required
 def update_product(product_id):
     product = Product.query.get(product_id)
     if not product:
