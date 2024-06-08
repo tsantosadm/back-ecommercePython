9dd20674666d4ea60047dba3141cec153dfe670a
Talisson Santos <tsantosadm@gmail.com>
2024-06-08 13:20:51 -0300

    router add product

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,6 +13,16 @@ class Product(db.Model):
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
+    
+#Definição de rota para requisitar cadastro
+@app.route('/api/products/add')
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data['name'], price=data['price'], description=data.get("description", ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"})
     
 #Definir uma rota raiz (paǵina inicial) e a função que será requisitada ao requisitar
 @app.route('/')
